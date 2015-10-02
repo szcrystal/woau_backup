0957053a3356896b576c059a61fbe6711a79486f
szk <szk.create@gmail.com>
2015-10-02 17:44:33 +0900

    woau server

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>--</title>
+        <title>Be Right Back</title>
 
         <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title"></div>
+                <div class="title">Be Right Back</div>
             </div>
         </div>
     </body>
